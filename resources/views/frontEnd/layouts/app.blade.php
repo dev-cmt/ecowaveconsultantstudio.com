1b52a0e73e84ebc6f2a8a6293b3546288b4b01a9
Motiur Rahman <motiur.cmt@gmail.com>
2025-08-31 23:59:35 +0600

    update

--- resources/views/frontEnd/layouts/app.blade.php
+++ resources/views/frontEnd/layouts/app.blade.php
@@ -1,154 +1,89 @@
 <!DOCTYPE html>
-<html lang="US">
-
+<html lang="en">
 <head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
 
-	<title>Resido - Residence & Real Estate HTML Template</title>
+<title>Ecowave Consultant Studio</title>
 
-	<!-- Custom CSS -->
-	<link href="{{asset('frontEnd')}}/css/styles.css" rel="stylesheet">
+<link rel="shortcut icon" href="{{asset('frontEnd')}}/img/logo.png" type="image/x-icon">
+<link rel="icon" href="{{asset('frontEnd')}}/img/logo.png" type="image/x-icon">
+<!-- Stylesheets -->
+<link href="{{asset('frontEnd')}}/css/bootstrap.css" rel="stylesheet">
+<link href="{{asset('frontEnd')}}/css/style.css" rel="stylesheet">
+<link href="{{asset('frontEnd')}}/css/responsive.css" rel="stylesheet">   
+<!--Color Switcher Mockup-->
+<link href="{{asset('frontEnd')}}/css/color-switcher-design.css" rel="stylesheet">
+<!--Color Themes-->
+<link id="theme-color-file" href="{{asset('frontEnd')}}/css/color-themes/default-theme.css" rel="stylesheet">
 
-	<!-- Custom Color Option -->
-	<link href="{{asset('frontEnd')}}/css/colors.css" rel="stylesheet">
-    @stack('css')
+@stack('styles')
+<!-- =======================================================
+    * Template Name: Sky Tech Solve
+    * Updated: Sep 10 2025 with Bootstrap v5.2.3
+    * Template URL: https://skytechsolve.com
+    * Author: Motiur Rahman (01909302126)
+    * License: https://skytechsolve.com/license/
+    ======================================================== -->
 </head>
-<body class="blue-skin">
-
-	<!-- ============================================================== -->
-	<!-- Preloader - style you can find in spinners.css -->
-	<!-- ============================================================== -->
-	<div id="preloader">
-		<div class="preloader"><span></span><span></span></div>
-	</div>
-
-	<!-- ============================================================== -->
-	<!-- Main wrapper - style you can find in pages.scss -->
-	<!-- ============================================================== -->
-	<div id="main-wrapper">
+<body>
+	<div class="page-wrapper">
+		<!-- Preloader -->
+		<div class="preloader"></div>
 
 		@include('frontEnd.include.header')
 
-        @yield('breadcrumb')
-        @yield('content')
+		@yield('breadcrumb')
+		@yield('content')
 
-        @include('frontEnd.include.footer')
-
-		<!-- Log In Modal -->
-		<div class="modal fade" id="login" tabindex="-1" role="dialog" aria-labelledby="registermodal"
-			aria-hidden="true">
-			<div class="modal-dialog modal-dialog-centered login-pop-form" role="document">
-				<div class="modal-content" id="registermodal">
-					<span class="mod-close" data-bs-dismiss="modal" aria-hidden="true">
-						<span class="svg-icon text-main svg-icon-2hx">
-							<svg width="32" height="32" viewBox="0 0 24 24" fill="none"
-								xmlns="http://www.w3.org/2000/svg">
-								<rect opacity="0.3" x="2" y="2" width="20" height="20" rx="10" fill="currentColor" />
-								<rect x="7" y="15.3137" width="12" height="2" rx="1" transform="rotate(-45 7 15.3137)"
-									fill="currentColor" />
-								<rect x="8.41422" y="7" width="12" height="2" rx="1" transform="rotate(45 8.41422 7)"
-									fill="currentColor" />
-							</svg>
-						</span>
-					</span>
-					<div class="modal-body">
-						<h4 class="modal-header-title">Log In</h4>
-						<div class="d-flex align-items-center justify-content-center mb-3">
-							<span class="svg-icon text-main svg-icon-2hx">
-								<svg width="80" height="80" viewBox="0 0 24 24" fill="none"
-									xmlns="http://www.w3.org/2000/svg">
-									<path
-										d="M15.8797 15.375C15.9797 15.075 15.9797 14.775 15.9797 14.475C15.9797 13.775 15.7797 13.075 15.4797 12.475C14.7797 11.275 13.4797 10.475 11.9797 10.475C11.7797 10.475 11.5797 10.475 11.3797 10.575C7.37971 11.075 4.67971 14.575 2.57971 18.075L10.8797 3.675C11.3797 2.775 12.5797 2.775 13.0797 3.675C13.1797 3.875 13.2797 3.975 13.3797 4.175C15.2797 7.575 16.9797 11.675 15.8797 15.375Z"
-										fill="currentColor" />
-									<path opacity="0.3"
-										d="M20.6797 20.6749C16.7797 20.6749 12.3797 20.275 9.57972 17.575C10.2797 18.075 11.0797 18.375 11.9797 18.375C13.4797 18.375 14.7797 17.5749 15.4797 16.2749C15.6797 15.9749 15.7797 15.675 15.7797 15.375V15.2749C16.8797 11.5749 15.2797 7.47495 13.2797 4.07495L21.6797 18.6749C22.2797 19.5749 21.6797 20.6749 20.6797 20.6749ZM8.67972 18.6749C8.17972 17.8749 7.97972 16.975 7.77972 15.975C7.37972 13.575 8.67972 10.775 11.3797 10.375C7.37972 10.875 4.67972 14.375 2.57972 17.875C2.47972 18.075 2.27972 18.375 2.17972 18.575C1.67972 19.475 2.27972 20.475 3.27972 20.475H10.3797C9.67972 20.175 9.07972 19.3749 8.67972 18.6749Z"
-										fill="currentColor" />
-								</svg>
-							</span>
-						</div>
-						<div class="login-form">
-							<form>
-
-								<div class="form-floating mb-3">
-									<input type="email" class="form-control" placeholder="name@example.com">
-									<label>Email address</label>
-								</div>
-
-								<div class="form-floating mb-3">
-									<input type="password" class="form-control" placeholder="Password">
-									<label>Password</label>
-								</div>
-
-								<div class="form-group mb-3">
-									<div class="d-flex align-items-center justify-content-between">
-										<div class="flex-shrink-0 flex-first">
-											<div class="form-check form-check-inline">
-												<input class="form-check-input" type="checkbox" id="save-pass"
-													value="option1">
-												<label class="form-check-label" for="save-pass">Save Password</label>
-											</div>
-										</div>
-										<div class="flex-shrink-0 flex-first">
-											<a href="#" class="link fw-medium">Forgot Password?</a>
-										</div>
-									</div>
-								</div>
-
-								<div class="form-group">
-									<button type="button"
-										class="btn btn-lg btn-main fw-medium full-width rounded-2">LogIn</button>
-								</div>
-
-							</form>
-						</div>
-						<div class="modal-divider"><span>Or login via</span></div>
-						<div class="social-login mb-3">
-							<ul>
-								<li><a href="#" class="btn connect-fb"><i class="bi bi-facebook"></i>Facebook</a></li>
-								<li><a href="#" class="btn connect-google"><i class="bi bi-google"></i>Google+</a></li>
-							</ul>
-						</div>
-						<div class="text-center">
-							<p class="mt-4">Have't Any Account? <a href="create-account.html"
-									class="link fw-medium">Acreate An Account</a></p>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-		<!-- End Modal -->
-
-		<a id="back2Top" class="top-scroll" title="Back to top" href="#"><i class="ti-arrow-up"></i></a>
+		@include('frontEnd.include.footer')
 
 	</div>
-	<!-- ============================================================== -->
-	<!-- End Wrapper -->
-	<!-- ============================================================== -->
 
-	<!-- ============================================================== -->
-	<!-- All Jquery -->
-	<!-- ============================================================== -->
-	<script src="{{asset('frontEnd')}}/js/jquery.min.js"></script>
+	<!-- Color Palate / Color Switcher -->
+	<div class="color-palate">
+		<div class="color-trigger">
+			<i class="fa fa-cog"></i>
+		</div>
+		<div class="color-palate-head">
+			<h6>Choose Your Color</h6>
+		</div>
+		<div class="various-color clearfix">
+			<div class="colors-list">
+				<span class="palate default-color active" data-theme-file="{{asset('frontEnd')}}/css/color-themes/default-theme.css"></span>
+				<span class="palate green-color" data-theme-file="{{asset('frontEnd')}}/css/color-themes/green-theme.css"></span>
+				<span class="palate blue-color" data-theme-file="{{asset('frontEnd')}}/css/color-themes/blue-theme.css"></span>
+				<span class="palate orange-color" data-theme-file="{{asset('frontEnd')}}/css/color-themes/orange-theme.css"></span>
+				<span class="palate purple-color" data-theme-file="{{asset('frontEnd')}}/css/color-themes/purple-theme.css"></span>
+				<span class="palate teal-color" data-theme-file="{{asset('frontEnd')}}/css/color-themes/teal-theme.css"></span>
+				<span class="palate brown-color" data-theme-file="{{asset('frontEnd')}}/css/color-themes/brown-theme.css"></span>
+				<span class="palate redd-color" data-theme-file="{{asset('frontEnd')}}/css/color-themes/redd-color.css"></span>
+			</div>
+		</div>
+		<ul class="box-version option-box"> <li>Full width</li> <li class="box">Boxed</li> </ul>
+		<ul class="rtl-version option-box"> <li>LTR Version</li> <li class="rtl">RTL Version</li> </ul>
+		<div class="palate-foo">
+			<span>You will find much more options for colors and styling in admin panel. This color picker is used only for demonstation purposes.</span>
+		</div>
+		<a href="#" class="purchase-btn">Purchase now $17</a>
+	</div><!-- End Color Switcher -->
+
+	<!--Scroll to top-->
+	<div class="scroll-to-top scroll-to-target" data-target="html"><span class="fa fa-arrow-circle-o-up"></span></div>
+
+	<script src="{{asset('frontEnd')}}/js/jquery.js"></script> 
 	<script src="{{asset('frontEnd')}}/js/popper.min.js"></script>
 	<script src="{{asset('frontEnd')}}/js/bootstrap.min.js"></script>
-	<script src="{{asset('frontEnd')}}/js/rangeslider.js"></script>
-	<script src="{{asset('frontEnd')}}/js/select2.min.js"></script>
-	<script src="{{asset('frontEnd')}}/js/jquery.magnific-popup.min.js"></script>
-	<script src="{{asset('frontEnd')}}/js/slick.js"></script>
-	<script src="{{asset('frontEnd')}}/js/slider-bg.js"></script>
-	<script src="{{asset('frontEnd')}}/js/lightbox.js"></script>
-	<script src="{{asset('frontEnd')}}/js/imagesloaded.js"></script>
+	<script src="{{asset('frontEnd')}}/js/jquery.fancybox.js"></script>
+	<script src="{{asset('frontEnd')}}/js/owl.js"></script>
+	<script src="{{asset('frontEnd')}}/js/wow.js"></script>
+	<script src="{{asset('frontEnd')}}/js/appear.js"></script>
+	<script src="{{asset('frontEnd')}}/js/mixitup.js"></script>
+	<script src="{{asset('frontEnd')}}/js/script.js"></script>
+	<!-- Color Setting -->
+	<script src="{{asset('frontEnd')}}/js/color-settings.js"></script>
 
-	<script src="{{asset('frontEnd')}}/js/custom.js"></script>
-
-	<!-- TYPED -->
-	<script src="{{asset('frontEnd')}}/js/typed.js"></script>
-	<script src="{{asset('frontEnd')}}/js/typed.init.js"></script>
-	<!-- ============================================================== -->
-	<!-- This page plugins -->
-	<!-- ============================================================== -->
-
-    @stack('js')
+	@stack('scripts')
 </body>
 </html>
